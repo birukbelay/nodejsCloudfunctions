6a643f7a93b9c2a91dffd732a66a9d3f31949fa1
Biruk Belay <56615724+birukbelay@users.noreply.github.com>
2021-06-03 22:31:28 +0300

    .

--- functions/util/config.js
+++ functions/util/config.js
@@ -3,12 +3,12 @@
 // TODO go to firebase and enable authentication
 // TODO in /functions folder  run npm install --save firebase
 module.exports =  {
-    apiKey: "AIzaSyDCd7XGBqQ8CvL8djmz_T_hpzAlxdwgaxA",
-    authDomain: "classed-33373.firebaseapp.com",
-    databaseURL: "https://classed-33373.firebaseio.com",
-    projectId: "classed-33373",
-    storageBucket: "classed-33373.appspot.com",
-    messagingSenderId: "127876999539",
-    appId: "1:127876999539:web:a0f549bcf3fa194db5bd06",
-    measurementId: "G-BYTRCLGNVH"
+    apiKey: process.env.apikey,
+    authDomain:  process.env.authDomain,
+    databaseURL:  process.env.databaseURL,
+    projectId:  process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId:  process.env.messagingSenderId,
+    appId:  process.env.appId,
+    measurementId:  process.env.measurementId,
   };
